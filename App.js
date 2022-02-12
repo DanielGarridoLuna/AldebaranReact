2ed4fd884513fe8fd20d279d54a6be85a8052d96
Daniel <draymonluna@gmail.com>
2022-02-12 11:49:50 -0600

    estilo en main

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SRadd from './Pantallas/SRadd'
 import SRread from './Pantallas/SRread'
 
+const styles = StyleSheet.create({
+  Boton: {
+    backgroundColor: '#FF0000',
+    width:500,
+  },
+});
+
+
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex:1, justifyContent: 'space-evenly',}}>
+    
       <Button
+        style={styles.Boton}
         title="Leer Información"
+        
         onPress={() => navigation.navigate('Leer')}
       />
 
       <Button
         title="Agregar Información"
+        style={styles.Boton}
         onPress={() => navigation.navigate('Agregar')}
       />
-
     </View>
   );
 }
@@ -51,3 +61,6 @@ function App() {
 }
 
 export default App;
+
+
+
