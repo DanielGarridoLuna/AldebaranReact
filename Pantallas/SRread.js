9ba4e8ddf77526fe5e76badf2a9c2e0b3ce20123
Daniel <draymonluna@gmail.com>
2022-02-12 11:59:36 -0600

    Leer y agrregar

--- Pantallas/SRread.js
+++ Pantallas/SRread.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { db } from '../Server/conexion';
+import { collection, getDocs } from "firebase/firestore";
 
 const SRread = () => {
+
+
+  async function leer(){
+    const querySnapshot = await getDocs(collection(db, "Productos"));
+
+        querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data().Producto}, $${doc.data().Precio}`);
+  });
+  }
+
+
     return (
         <View style={styles.container}>
           <Text>Lee información</Text>
-       
+          <Button title="Leer" onPress={() =>leer()}>Leer</Button>
         </View>
       )
 }
